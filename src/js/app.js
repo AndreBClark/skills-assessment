59845c9a753e4c5f98029c127f46bd2214b4faf5
AndreBClark <andre@cosmicdivision.dev>
2020-02-11 20:03:40 -0800

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,111 @@
+const view = {
+    status: {
+        online: 'green',
+        away: 'yellow',
+        offline: 'red',
+        busy: 'goldenRod',
+    },
+    app: document.querySelector('#app'),
+};
 
+const model = [{
+        status: view.status.online,
+        name: 'Christian',
+        email: 'christian@yahoo.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.online,
+        name: 'Rich',
+        email: 'rich@tripod.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.online,
+        name: 'Scott',
+        email: 'scott@mailinator.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.online,
+        name: 'Danny',
+        email: 'danny@hotmail.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.offline,
+        name: 'Taka',
+        email: 'taka@myspace.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.away,
+        name: 'Tim',
+        email: 'tim@netscape.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.online,
+        name: 'Patrick',
+        email: 'patrick@live.com',
+        phone: '323-555-1234',
+    },
+    {
+        status: view.status.busy,
+        name: 'Jacques',
+        email: 'jacques@aol.com',
+        phone: '323-555-1234',
+    },
+];
+
+// function ToggleEachClass(selectedClassesString, toggledClassString) {
+//     let Elements = document.querySelectorAll(selectedClassesString);
+//     [].forEach.call(Elements, el => el.classList.toggle(toggledClassString));
+// }
+function addToEachClass(selectedClassesString, callFunction) {
+    let Elements = document.querySelectorAll(selectedClassesString);
+    [].forEach.call(Elements, callFunction);
+}
+addToEachClass('article', el => el.classList.toggle('hidden'));
+const dropdown = document.querySelector('#selectDetail');
+dropdown.onchange = function() {
+    if (dropdown.options.selectedIndex === 1) {
+        addToEachClass('.phone-number', el => el.classList.toggle('hidden'));
+        addToEachClass('.email', el => el.classList.toggle('hidden'));
+    } else {
+        addToEachClass('.email', el => el.classList.toggle('hidden'));
+        addToEachClass('.phone-number', el => el.classList.toggle('hidden'));
+    }
+};
+
+
+function CreateRows(HTMLtag, ParentHTMLtag) {
+    for (let i = 0; i < model.length; i++) {
+        var newElement = document.createElement(HTMLtag);
+        newElement.innerHTML = `
+<div class="left-column">
+<svg class="status-indicator" fill="${model[i].status}">
+<use xlink:href="#statusIndicator" />
+</svg>
+<p class="name">${model[i].name}</p>
+</div>
+<button class="right-column contact-info">
+<div class="contact-info__brief">
+<p class="email">${model[i].email}</p>
+<p class="hidden phone-number">323-555-1234</p>
+</div>
+<div class="hidden roll-over contact-info__expanded">
+<p class="email">${model[i].email}</p>
+<p class="phone-number">${model[i].phone}</p>
+<p class="address">
+6539 Wilton Ave. <br />
+Culver City CA 90234
+</p>
+</div>
+</button>
+        `;
+        const Parent = document.querySelector(ParentHTMLtag);
+        Parent.appendChild(newElement);
+    }
+}
+document.body.onload = CreateRows('article', '#infoList');
